2a3e240d4ffa0fb8029fe756ec2a21c39923c948
mattlusty <52744525+mattlusty@users.noreply.github.com>
2024-01-08 23:27:27 +0000

    Update 1c) Vectors (as objects).js

--- Objects (aka Structs)/1c) Vectors (as objects).js
+++ Objects (aka Structs)/1c) Vectors (as objects).js
@@ -4,7 +4,7 @@
 
 // Simple object/struct (representing a vector)
 
-var myVector1 = {
+var _myVector1 = {
   x: 3,
   y: 8
 }
@@ -44,12 +44,12 @@ myVector2.y = 20;
 // Interesting fact, when GMS tries to convert an object to a string (using the string() function)
 // - It first checks to see if there is a custom toString() method on the object 
 // - Hence why we see our custom toString() output! ...
-show_message(string(_myVector2)); // "{ x is ... 10, y is ... 20 }"
+show_message(string(myVector2)); // "{ x is ... 10, y is ... 20 }"
 
 // Call the myVector2 object's "add" method
 myVector2.add(myVector1) // alters the myVector2's x, y properties!
 
-show_message(string(_myVector2)); // "{ x is ... 15, y is ... 28 }"
+show_message(string(myVector2)); // "{ x is ... 15, y is ... 28 }"
 
 // So its the same result as ...
-show_message(_myVector2.toString()); // "{ x is ... 15, y is ... 28 }"
+show_message(myVector2.toString()); // "{ x is ... 15, y is ... 28 }"
